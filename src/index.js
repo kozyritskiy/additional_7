5289e010d22917797349246de6cc6f763b6066da
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-09-23 14:57:28 +0300

    one test pass

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ module.exports = function solveSudoku(matrix) {
 
   while (valid === false) {
     // Поиск первого нуля
-    for (var i = 0; i < matrix.length; i++) {
-      for (var j = 0; j < matrix[i].length; j++) {
+    loop1:
+    for (let i = 0; i < matrix.length; i++) {
+      loop2:
+      for (let j = 0; j < matrix[i].length; j++) {
         if (matrix[i][j] === 0) {
           row = matrix[i];
           cordX = j;
           cordY = i;
-          break;
+          break loop1;
         }
         else {
           countOfValid++;
@@ -24,18 +26,19 @@ module.exports = function solveSudoku(matrix) {
 
     if (countOfValid === 81) {
       valid = true;
+      break;
     }
 
     column = [];
     // Колонка в которой был найден ноль
-    for (var i = 0; i < matrix.length; i++) {
+    for (let i = 0; i < matrix.length; i++) {
       column.push(matrix[i][cordX]);
     }
 
     // поиск числа которого нету в строке
     function findInRow(j) {
       let tempArr = 0;
-      for (var i = 0; i < row.length; i++) {
+      for (let i = 0; i < row.length; i++) {
         if (row[i] !== j) {
           tempArr++;
         }
@@ -46,27 +49,54 @@ module.exports = function solveSudoku(matrix) {
     }
     // поиск числа которого нету в столбце
     function findInColumn(j) {
-      let tempArr1 = 0;
-      for (var i = 0; i < column.length; i++) {
+      let tempArr = 0;
+      for (let i = 0; i < column.length; i++) {
         if (column[i] !== j) {
-          tempArr1++;
+          tempArr++;
         }
       }
-      if (tempArr1 === 9) {
+      if (tempArr === 9) {
         return true;
       }
     }
+    function findInSector(j) {
+      let sectorX = parseInt(cordX / 3) * 3;
+      let sectorY = parseInt(cordY / 3) * 3;
+      let sector = [];
+      let cntX = sectorX + 3;
+      let cntY = sectorY + 3;
+      for (sectorX; sectorX < cntX; sectorX++) {
+        sectorY = parseInt(cordY / 3) * 3;
+        for (sectorY; sectorY < cntY; sectorY++) {
+          sector.push(matrix[sectorY][sectorX]);
+        }
+      }
+      let tempArr = 0;
+      for (let i = 0; i < sector.length; i++) {
+        if (sector[i] !== j) {
+          tempArr++;
+        }
+      }
+      if (tempArr === 9) {
+        return true;
+      }
+    }
+
     // Поиск кандидата
     let chislo = 0;
     for (let i = 1; i <= 9; i++) {
       let c = findInColumn(i);
       let r = findInRow(i);
-      if (c && r === true) {
+      let o = findInSector(i);
+      if (c && r && o) {
         chislo = i;
         break;
       }
     }
-    if (countOfValid !== 81) {
+    if (chislo === 0){
+      break;
+    }
+    if (countOfValid !== 81 && chislo !==0) {
       matrix[cordY][cordX] = chislo;
     }
     countOfValid = 0;
